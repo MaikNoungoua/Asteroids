fdaf81764c48c02f01eb21dac73c1740f5d91051
Maik <noungouamaik2023@outlook.com>
2024-12-31 15:33:02 +0000

    C: moving around and back, back and forth done

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 
 import pygame 
 from constants import *
+from player import Player
 
 
 
@@ -12,16 +13,21 @@ def main():
     print("Starting asteroids!")
     pygame.init() #used to initialize the game 
     time_clock = pygame.time.Clock()
-    dt = 0 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0 
+
     while True: 
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return 
+        player.update(dt)
         screen.fill((0,0,0))
+        player.draw(screen)
         pygame.display.flip()
-        dt = time_clock.tick(60)/1000 #pause the game loop until 1/60th of a second has passed
+        #limit the framerate to 60 FPS
+        dt = time_clock.tick(60) / 1000
 
 
 if __name__ == "__main__": 
